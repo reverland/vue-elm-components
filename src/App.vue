5226943b78ac06d440b73e63e1bbab8ca0abed21
Yuyang Liu <yuyang.liu@chaitin.com>
2016-11-24 19:22:00 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app">
+    <elm :src="ElmButton"></elm>
+  </div>
+</template>
+
+<script>
+import ElmButton from './elm/Button.elm'
+import Elm from './components/Elm'
+
+export default {
+  name: 'App',
+  components: {
+    Elm
+  },
+  data () {
+    return {
+      ElmButton: ElmButton
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Elm.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="elm">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Elm',
+  props: {
+    src: {
+      type: Object
+    },
+    flags: {
+      type: Object
+    },
+    ports: {
+      type: Object
+    }
+  },
+  mounted: function () {
+    var app = this.src.Main.embed(this.$el, this.flags)
+
+    if (typeof this.ports !== 'undefined') {
+      this.ports(app.ports)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
